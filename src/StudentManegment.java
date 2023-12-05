12dda585d415a884f89d6b15c622a94f9d211eeb
sarraRE <rejebsarra@isima.u-monastir.tn>
2023-12-05 12:24:54 +0100

    prosit 12

---
+++ src/StudentManegment.java
@@ -0,0 +1,43 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StudentManegment implements Management{
+
+    @Override
+    public void displayStudents(List<Etudiant> etudiants, Consumer<Etudiant> con) {
+         etudiants.forEach(con);
+    }
+
+    @Override
+    public void displayStudentsByFilter(List<Etudiant> students, Predicate<Etudiant> pre, Consumer<Etudiant> con) {
+        students.stream().filter(pre).forEach(con);
+    }
+
+    @Override
+    public String returnStudentsNames(List<Etudiant> students, Function<Etudiant, String> fun) {
+        StringBuilder names = new StringBuilder();
+        students.forEach(student -> names.append(fun.apply(student)).append(" "));
+        return names.toString();
+    }
+
+    @Override
+    public Etudiant createStudent(Supplier<Etudiant> sup) {
+        return sup.get();
+    }
+
+    @Override
+    public List<Etudiant> sortStudentsById(List<Etudiant> students, Comparator<Etudiant> com) {
+        students.sort(com);
+        return students;
+    }
+
+    @Override
+    public Stream<Etudiant> convertToStream(List<Etudiant> students) {
+        return students.stream();
+    }
+}
